f791d3686d5a13b7a5b3f9f7bf77018914f9f7cd
Michal Burda <michal.burda3@gmail.com>
2017-01-15 18:39:05 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#5cbdaa',
     lineColor: '#5cbdaa',
-    // density: 100;
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
